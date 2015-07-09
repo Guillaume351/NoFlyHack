020e39709c8d912ad8e0296d7ce1b6d3b0098fd3
Guillaume351 <guillaume.claverie@mail.com>
2015-07-09 22:30:54 +0200

    Update MainClass.php

--- src/NoFlyHack/MainClass.php
+++ src/NoFlyHack/MainClass.php
@@ -15,32 +15,29 @@ use pocketmine\utils\TextFormat;
 
 class MainClass extends PluginBase implements Listener{
     public $players = [];
-	public function onLoad(){
+    
+    public function onLoad(){}
 
-	}
+    public function onEnable(){
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
 
-	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-    }
-
-	public function onDisable(){
-
-	}
+    public function onDisable(){}
 
     public function onPlayerJoin(PlayerJoinEvent $event){
         $this->players[$event->getPlayer()->getName()] = 0;
-    }
+}
 
-	public function PlayerMove(PlayerMoveEvent $event){
+    public function PlayerMove(PlayerMoveEvent $event){
         if ($event->getPlayer()->getY() < 24  and $event->getPlayer()->getGamemode() != 1  and$event->getPlayer()->onGround == false){
             $this->players[$event->getPlayer()->getName()] += 1;
         }else{
             $this->players[$event->getPlayer()->getName()] = 0;
         }
-
         if($this->players[$event->getPlayer()->getName()] > 48){
             $event->getPlayer()->kick("Fly detected.".TextFormat::RED." \nPlease disable all mods.");
         }
-	}
+}
+
+	
 }
