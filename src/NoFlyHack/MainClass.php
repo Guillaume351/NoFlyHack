e704013320d4a9d56874e5dfbe6dc7112cab00d6
Guillaume351 <guillaume.claverie@mail.com>
2015-10-11 15:28:55 +0200

    Change Fly Detection Method

--- src/NoFlyHack/MainClass.php
+++ src/NoFlyHack/MainClass.php
@@ -4,10 +4,12 @@ namespace NoFlyHack;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
@@ -15,29 +17,29 @@ use pocketmine\utils\TextFormat;
 
 class MainClass extends PluginBase implements Listener{
     public $players = [];
+    public $chances = [];
+    public $numberOfPacket = [];
+
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
     
-    public function onLoad(){}
-
-    public function onEnable(){
-	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-}
-
-    public function onDisable(){}
-
     public function onPlayerJoin(PlayerJoinEvent $event){
         $this->players[$event->getPlayer()->getName()] = 0;
-}
+    }
 
-    public function PlayerMove(PlayerMoveEvent $event){
-        if ($event->getPlayer()->getY() < 24  and $event->getPlayer()->getGamemode() != 1  and $event->getPlayer()->getGamemode() != 3 and $event->getPlayer()->onGround == false){
-            $this->players[$event->getPlayer()->getName()] += 1;
+	public function PlayerMove(PlayerMoveEvent $event){
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        if(round($event->getTo()->getY() - $event->getFrom()->getY(),3) == 0.375) {
+            $this->players[$name] ++;
         }else{
-            $this->players[$event->getPlayer()->getName()] = 0;
+            $this->players[$name] = 0;
         }
-        if($this->players[$event->getPlayer()->getName()] > 48){
-            $event->getPlayer()->kick("Fly detected.".TextFormat::RED." \nPlease disable all mods.");
+        if($this->players[$name] === 3){
+            $this->getServer()->broadcastMessage(TextFormat::RED.$name. TextFormat::YELLOW." banned for ".TextFormat::RED."FLY HACK");
+            $event->getPlayer()->kick("Fly detected.".TextFormat::RED."  You have been IP banned.");
+            $this->getServer()->getIPBans()->addBan($player->getAddress());
         }
-}
-
-	
+	}
 }
